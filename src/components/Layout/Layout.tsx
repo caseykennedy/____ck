0be41f3eff4f4b8c3ae06e1ccff9a5f51e44e0d1
Casey K <tehcnicolour@gmail.com>
2020-03-15 19:47:01 -0700

    removes template

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,15 +3,10 @@
 
 // ___________________________________________________________________
 
-import React, { useEffect, useRef } from 'react'
+import React, { useRef } from 'react'
 
 import { ThemeProvider } from 'styled-components'
-import { useSpring } from 'react-spring'
 
-import useNavigation from '../../hooks/useNavigation'
-
-import Header from '../Header'
-import Footer from '../Footer'
 import Cursor from '../Cursor'
 
 import theme from '../../../config/theme'
@@ -25,41 +20,12 @@ type LayoutProps = {
   children: React.ReactNode
 }
 
-// Parallax
-// https://dev.to/robbertvancaem/
-// https://www.pluralsight.com/guides/using-react-refs-typescript
-// You can use this `calc` method to increase the impact
-// of the effect by playing around with the values and units.
-const calcTranslate = (o: any) => `translateY(${o * -0.3}px)`
-
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  // Navigation data hook
-  const navData = useNavigation()
-
   // Ref <main> to lock body on scroll
   const mainRef = useRef<HTMLDivElement>(null)
 
   // Parallax scroll
   const scrollRef = useRef<HTMLDivElement>(null)
-  const [{ offset }, set] = useSpring(() => ({ offset: 0 }))
-  const handleScroll = () => {
-    // Need to figure this type error out
-    const posY = scrollRef.current.getBoundingClientRect().top
-    const offset = window.pageYOffset - posY
-
-    set({ offset })
-  }
-
-  useEffect(() => {
-    // Parallax scroll
-    window.addEventListener('scroll', handleScroll)
-
-    // Remove event listener on cleanup
-    return () => {
-      // Parallax scroll
-      window.removeEventListener('scroll', handleScroll)
-    }
-  })
 
   // eslint-disable-next-line no-console
   console.log(`%c (+)(—) core37 `, `background: #000000; color: #ffffff`)
@@ -68,15 +34,10 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       <GlobalStyles />
       <Cursor />
       <S.Wrapper ref={scrollRef}>
-        {/* <Header mainRef={mainRef} navData={navData} /> */}
         <S.Main
           ref={mainRef}
-          // style={{
-          //   transform: offset.interpolate(calcTranslate)
-          // }}
         >
           {children}
-          {/* <Footer navData={navData} /> */}
         </S.Main>
       </S.Wrapper>
     </ThemeProvider>
--- src/components/Layout/styles.scss.tsx
+++ src/components/Layout/styles.scss.tsx
@@ -9,9 +9,6 @@ import { animated } from 'react-spring'
 // Theme
 import theme from '../../../config/theme'
 
-// Elements
-import { AnimatedBox } from '../../elements'
-
 // Begin Styles
 // ___________________________________________________________________
 
@@ -29,13 +26,7 @@ export const Wrapper = styled.div`
 export const Main = styled(animated.main)`
   max-height: 100vh;
   margin-bottom: 0rem;
-  /* min-width: 0; slick-slider overflow fix */
-
   width: ${theme.siteWidth};
-
-  /* @media ${theme.mq.medium} {
-    width: calc(100vw - ${theme.sidebarWidth.collapsed});
-  } */
 `
 
 // ___________________________________________________________________ Styles
