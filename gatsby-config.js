63f908e0912e7715d1a13587b15ca4d1651547f1
Casey K <tehcnicolour@gmail.com>
2020-03-15 19:54:07 -0700

    config cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,22 +37,8 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        name: 'projects',
-        path: `${__dirname}/content/projects`,
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
         name: 'config',
         path: `${__dirname}/config`,
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: `${__dirname}/src/images`,
       },
     },
     {
