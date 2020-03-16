8f52252e68a3c34bc344317a743505b9122a946e
Casey K <tehcnicolour@gmail.com>
2020-03-15 19:31:40 -0700

    rm unused libraries

--- src/components/Layout/styles.scss.tsx
+++ src/components/Layout/styles.scss.tsx
@@ -5,7 +5,6 @@
 // Libraries
 import styled from 'styled-components'
 import { animated } from 'react-spring'
-import { lighten } from 'polished'
 
 // Theme
 import theme from '../../../config/theme'
@@ -37,46 +36,6 @@ export const Main = styled(animated.main)`
   /* @media ${theme.mq.medium} {
     width: calc(100vw - ${theme.sidebarWidth.collapsed});
   } */
-`
-
-export const Footer = styled.footer<{ color?: string[] }>`
-  position: fixed;
-  width: ${theme.sidebarWidth.collapsed};
-  bottom: 0;
-  background: ${theme.colors.primary};
-  color: ${lighten(0.3, `${theme.colors.primary}`)};
-
-  a {
-    color: ${lighten(0.3, `${theme.colors.primary}`)};
-    text-decoration: none;
-
-    &:hover {
-      color: ${lighten(0.6, `${theme.colors.primary}`)};
-    }
-  }
-
-  .ico {
-    margin-right: ${theme.space[2]};
-    width: ${theme.space[4]};
-    cursor: pointer;
-
-    svg {
-      fill: ${theme.colors.white};
-
-      &:hover {
-        fill: ${lighten(0.3, `${theme.colors.primary}`)};
-      }
-    }
-  }
-
-  @media (max-width: ${theme.breakpoints[4]}) {
-    width: ${theme.sidebarWidth.normal};
-  }
-
-  @media (max-width: ${theme.breakpoints[2]}) {
-    position: relative;
-    width: 100%;
-  }
 `
 
 // ___________________________________________________________________ Styles
